dc3238c30bbe607adb493c87cdb47949a2725001
HassanRaza-313 <163582928+HassanRaza-313@users.noreply.github.com>
2025-02-11 15:38:01 +0500

    Initial commit

---
+++ lib/wrapper.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:login_signup_firebase_auth/homepage.dart';
+import 'package:login_signup_firebase_auth/login.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({super.key});
+
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context,snapshot){
+            if(snapshot.hasData){
+              return HomePage();
+            }else{
+              return Login();
+            }
+          }),
+    );
+  }
+}
